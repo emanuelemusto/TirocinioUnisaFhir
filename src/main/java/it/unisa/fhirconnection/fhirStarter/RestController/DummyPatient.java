3d670314066c66917575704ba056887cffd018f5
emanuelemusto <emanueleugo.musto@gmail.com>
2020-12-08 20:47:27 +0100

    add function for adding patient

---
+++ src/main/java/it/unisa/fhirconnection/fhirStarter/RestController/DummyPatient.java
@@ -0,0 +1,133 @@
+package it.unisa.fhirconnection.fhirStarter.RestController;
+
+import lombok.NoArgsConstructor;
+
+
+public class DummyPatient {
+    private int id;
+    private String firstname;
+    private String familyname;
+    private String gender;
+    private String date;
+    private String telecomValue;
+    private String telecomUse;
+    private String addressLine;
+    private String city;
+    private String postCode;
+    private String country;
+    private String addressUse;
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getAddressLine() {
+        return addressLine;
+    }
+
+    public void setAddressLine(String addressLine) {
+        this.addressLine = addressLine;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getFamilyname() {
+        return familyname;
+    }
+
+    public void setFamilyname(String familyname) {
+        this.familyname = familyname;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getTelecomValue() {
+        return telecomValue;
+    }
+
+    public void setTelecomValue(String telecomValue) {
+        this.telecomValue = telecomValue;
+    }
+
+    public String getTelecomUse() {
+        return telecomUse;
+    }
+
+    public void setTelecomUse(String telecomUse) {
+        this.telecomUse = telecomUse;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPostCode() {
+        return postCode;
+    }
+
+    public void setPostCode(String postCode) {
+        this.postCode = postCode;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getAddressUse() {
+        return addressUse;
+    }
+
+    public void setAddressUse(String addressUse) {
+        this.addressUse = addressUse;
+    }
+
+    @Override
+    public String toString() {
+        return "DummyPatient{" +
+                "id=" + id +
+                ", firstname='" + firstname + '\'' +
+                ", familyname='" + familyname + '\'' +
+                ", gender='" + gender + '\'' +
+                ", date='" + date + '\'' +
+                ", telecomValue='" + telecomValue + '\'' +
+                ", telecomUse='" + telecomUse + '\'' +
+                ", addressLine='" + addressLine + '\'' +
+                ", city='" + city + '\'' +
+                ", postCode='" + postCode + '\'' +
+                ", country='" + country + '\'' +
+                ", addressUse='" + addressUse + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/it/unisa/fhirconnection/fhirStarter/RestController/PatientRestController.java
@@ -0,0 +1,19 @@
+package it.unisa.fhirconnection.fhirStarter.RestController;
+
+import it.unisa.fhirconnection.fhirStarter.controller.PatientController;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin(origins = "*")
+public class PatientRestController {
+    @RequestMapping(value = "addpatient", method = RequestMethod.POST)
+    public static void addEmployee(@RequestBody DummyPatient patient) {
+        PatientController.addPatient(patient);
+        System.out.println(patient.toString());
+
+    }
+    @RequestMapping(value = "printcose", method = RequestMethod.GET)
+    public static void getAllEmployees(){
+        System.out.println("ci sono ");
+    }
+}
